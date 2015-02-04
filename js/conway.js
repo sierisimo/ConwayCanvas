72a82275ab4445cbe433ae2376ea0cf1f74438c4
Sier Violencia <sierisimo@gmail.com>
2015-02-04 11:59:43 -0600

    Html basico

---
+++ js/conway.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded',function(){
+  var canvas = document.getElementById("conway"),
+    canWidth = canvas.offsetWidth,
+    canHeight = canvas.offsetHeight;
+});
+
+var canvasClass = {
+  Square: function(){
+    this.alive = false;
+    
+  }
+};
