b143ce3dbd2ee12721c06203bbdd27ffbe0ad81c
Sier Violencia <sierisimo@gmail.com>
2015-02-05 01:40:03 -0600

    Added d3... possibly changing name to: 'ConwaySVG'

--- js/conway.js
+++ js/conway.js
@@ -1,12 +1,32 @@
+//TODO: Change this for a function that can be called
+/*
 document.addEventListener('DOMContentLoaded',function(){
   var canvas = document.getElementById("conway"),
+    canHeight = canvas.offsetHeight,
     canWidth = canvas.offsetWidth,
-    canHeight = canvas.offsetHeight;
+    squares = 10, //TODO: Change this for a variable that could be setted
+    ctx = canvas.getContext("2d");
+
+    ctx.lineWidth = 1;
+    ctx.beginPath();
+    ctx.moveTo(0,0);
+    ctx.lineTo(0,140);
+    ctx.stroke();
+
+    //for (var i = 0; i < 10; i++){
+    //ctx.lineWidth = 1+i;
+    //ctx.beginPath();
+    //ctx.moveTo(5+i*14,5);
+    //ctx.lineTo(5+i*14,140);
+    //ctx.stroke();
+  //}
+    window.ctx = ctx;
 });
+*/
 
 var canvasClass = {
   Square: function(){
     this.alive = false;
-    
+
   }
 };
