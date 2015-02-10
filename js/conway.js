624e909685c9ab73d857298071edfe927c32ca37
Sier Violencia <sierisimo@gmail.com>
2015-02-10 00:45:07 -0600

    Before function of play

--- js/conway.js
+++ js/conway.js
@@ -1,41 +1,71 @@
 document.addEventListener('DOMContentLoaded',function(){
-  var width = 600, height = width,
+  var width = 600, height = width, //Simple values for all svg, change it if you want bigger or smaller svg
+    btn = document.getElementById('play'),
     svg = d3.select("#container").append("svg")
       .attr("width",width).attr("height",height),
     dataset = (function(){
+      //Change the 10 for the number of elements you want in the game.
       var r = [], t = width%10, p = height%10,
         tw = (width-t)/10, th = (height-p)/10;
 
+      // Create the square. That easy
       for(var i = 0; i < width-t; i+=tw){
-        for(var j = 0; j < height-p; j+=th)
-          r.push(new figClass.Square(i,j));
+        for(var j = 0; j < height-p; j+=th){
+          //The conway.Square contains the real data, we push it into the array
+          // because d3 expects an array of data. for elements
+          r.push(new conway.Square(i,j,tw));
+        }
       }
       return r;
     })(),
     rects = svg.selectAll("rect")
       .data(dataset).enter().append("rect");
-    /*
-    circles = svg.selectAll("circle")
-     .data(dataset)
-     .enter()
-     .append("circle");
 
-  circles.attr('cx',function(d, i) {
-            return (i * 50) + 25;
-        })
-       .attr("cy", height/2)
-       .attr("r", function(d) {
-            return d;
-       });
-     */
-     console.log(rects);
+  rects.attr("width",function(d){
+    return d.l;
+  }).attr("height",function(d){
+    return d.l;
+  }).attr("x",function(d){
+    return d.x;
+  }).attr("y",function(d){
+    return d.y;
+  }).attr("class",function(d){
+    //Check conway.css and _svg.scss
+    if(d.alive){
+      return "alive";
+    }
+    return "dead";
+  });
+
+  //Let the user choose the initial config
+  rects.on("click",function(d){
+    if(this.classList.contains("dead")){
+      this.classList.remove("dead");
+      this.classList.add("alive");
+      d.alive = true;
+    }else{
+      this.classList.remove("alive");
+      this.classList.add("dead");
+      d.alive = false;
+    }
+  })
+
+  //Prevent the user change the config after started
+  btn.addEventListener('click',function(){
+    rects.on("click",null);
+    conway.play(rects);
+  });
+
 });
 
-
-var figClass = {
-  Square: function(x,y){
+var conway = {
+  Square: function(a,b,c){
     this.alive = false;
-    this.x = x;
-    this.y = y;
+    this.__defineGetter__("x",function(){return a;});
+    this.__defineGetter__("y",function(){return b;});
+    this.__defineGetter__("l",function(){return c});
+  },
+  play: function(rects){
+
   }
 };
